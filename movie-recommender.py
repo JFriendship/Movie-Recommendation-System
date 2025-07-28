fdf53098b2f61518f4db935f9eeb3c81514e600f
JFriendship <jackfriendshipusa@gmail.com>
2025-07-28 14:10:56 -0700

    Add recommend_movies function
    Added recommend_movies function and fixed a bug that was causing zero-sum user profiles, not enough recommendations, and invalid movie recommendations.

--- movie-recommender.py
+++ movie-recommender.py
@@ -2,6 +2,23 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
+def recommend_movies(user_id, num_recommendations=5, movies_rated_by_user=None):
+    user_similarities = df_user_movie_similarities.loc[user_id]
+
+    if movies_rated_by_user is None:
+        movies_rated_by_user = df_ratings[df_ratings['userId'] == user_id]['movieId'].values
+
+    movies_rated_by_user = movies_rated_by_user[np.isin(movies_rated_by_user, df_movies['movieId'])]
+
+    available_movies = user_similarities.drop(index=movies_rated_by_user, errors='ignore')
+    print(f"Available movies for recommendation: {len(available_movies)}")
+
+    df_recommendations = user_similarities.drop(index=movies_rated_by_user, errors='ignore').sort_values(ascending=False).head(num_recommendations)
+
+    movie_subset = df_movies[['movieId', 'title']]
+    df_recommendations = df_recommendations.reset_index(name='score').rename(columns={'index': 'movieId'})
+    return movie_subset.merge(df_recommendations, on='movieId')
+
 # -=| Data Loading |=-
 movies_file_path = 'Dataset/movies.csv'
 ratings_file_path = 'Dataset/ratings.csv'
@@ -27,7 +44,11 @@ movie_replacement_map = {
     64997: 34048
 }
 # df_ratings['movieId'].replace(bad_movie_ids, replacement_movie_ids)
-df_ratings['movieId'].replace(movie_replacement_map)
+df_ratings['movieId'] = df_ratings['movieId'].replace(movie_replacement_map)
+
+# Removing movies with "(no genres listed)"
+df_movies = df_movies[df_movies['genres'] != '(no genres listed)']
+df_ratings = df_ratings[df_ratings['movieId'].isin(df_movies['movieId'])]
 
 # Remove less active users and movies
 user_rating_threshold = 20
@@ -39,10 +60,13 @@ ratings_per_movie_threshold = 10
 movie_rating_counts = df_ratings['movieId'].value_counts()
 movies_to_keep = movie_rating_counts[movie_rating_counts >= ratings_per_movie_threshold].index
 df_ratings = df_ratings[df_ratings['movieId'].isin(movies_to_keep)]
+df_movies = df_movies[df_movies['movieId'].isin(movies_to_keep)]    # Have to remove them from both dataframes
 
 # Multi-hot Encoding Genres
 genre_split = df_movies['genres'].str.split('|')
 df_genres = (genre_split.explode().str.strip().pipe(pd.get_dummies).groupby(level=0).sum())
+
+df_genres = df_genres.set_index(df_movies['movieId'])
 
 df_ratings = df_ratings.merge(df_movies[['movieId', 'title']], on='movieId')
 df_ratings_with_genres = df_ratings.join(df_genres, on='movieId')
@@ -51,6 +75,13 @@ user_profiles = df_ratings_with_genres.groupby('userId')[np.array(df_genres.colu
 user_profiles = pd.DataFrame(user_profiles.tolist(), index=user_profiles.index, columns=np.array(df_genres.columns.tolist()))
 user_profiles = user_profiles.fillna(0) # assume preference for a genre is 0 if the user hasn't rated a movie in said genre
 
-similarity_matrix = cosine_similarity(user_profiles.values, df_genres.values)
-df_similarities = pd.DataFrame(similarity_matrix, index=user_profiles.index, columns=df_genres.index)
+print(df_ratings_with_genres.head())
 
+# print("User profile for user 2:\n", user_profiles.loc[2])
+# print("Sum of profile vector:", user_profiles.loc[2].sum())
+
+similarity_matrix = cosine_similarity(user_profiles.values, df_genres.values)
+df_user_movie_similarities = pd.DataFrame(similarity_matrix, index=user_profiles.index, columns=df_genres.index)
+
+recommendations = recommend_movies(user_id=4, num_recommendations=10)
+print(recommendations)
