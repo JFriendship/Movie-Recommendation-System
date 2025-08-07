47d6ff04a480dfeca653e02e3a6eb42feb516b15
JFriendship <jackfriendshipusa@gmail.com>
2025-08-07 10:57:44 -0700

    Add recall_at_k and average_recall_at_k Functions
    Added recall_at_k and average_recall_at_k functions to a new fille called evaluation.py. Also removed some code that was no longer of use.

--- movie-recommender.py
+++ movie-recommender.py
@@ -1,35 +1,6 @@
-import numpy as np
-import pandas as pd
-from sklearn.metrics.pairwise import cosine_similarity
 import recommender.preprocessing as pp
 import recommender.model as model
-
-# def recommend_movies(user_id, num_recommendations=10):
-#     # Get the similarity vector for this user
-#     user_similarities = df_user_movie_similarities.loc[user_id]
-
-#     # Exclude only training items
-#     train_movies = df_train_ratings[df_train_ratings['userId'] == user_id]['movieId'].values
-#     available_movies = user_similarities.drop(index=train_movies, errors='ignore')
-
-#     # Recommend top-k items
-#     top_k = available_movies.sort_values(ascending=False).head(num_recommendations)
-#     top_k = top_k.reset_index(name='score').rename(columns={'index': 'movieId'})
-
-#     return df_movies[['movieId', 'title']].merge(top_k, on='movieId')
-
-# def recommend_movies_all_users(num_recommendations=10):
-#     all_recommendations = {}
-#     for user_id in df_test_ratings['userId'].unique():
-#         recommendations = model.recommend_movies(user_id=user_id, num_recommendations=num_recommendations)
-#         all_recommendations[user_id] = recommendations['movieId'].tolist()
-#     return all_recommendations
-
-def recall_at_k(recommended_items, relevant_items):
-    if not relevant_items:
-        return None
-    hits = len(set(recommended_items) & set(relevant_items))
-    return hits / len(relevant_items)
+import recommender.evaluation as eval
 
 # -=| Data Loading |=-
 df_movies, df_ratings = pp.load_data('data/movies.csv', 'data/ratings.csv')
@@ -55,15 +26,9 @@ df_genres = pp.encode_genres(df_movies=df_movies)
 
 user_profiles = model.create_user_profiles(df_ratings=df_train_ratings, df_movies=df_movies, df_genres=df_genres)
 
-# print("User profile for user 2:\n", user_profiles.loc[2])
-# print("Sum of profile vector:", user_profiles.loc[2].sum())
-
-# similarity_matrix = cosine_similarity(user_profiles.values, df_genres.values)
-# df_user_movie_similarities = pd.DataFrame(similarity_matrix, index=user_profiles.index, columns=df_genres.index)
 df_user_movie_similarities = model.compute_similarity_matrix(user_profiles=user_profiles, df_genres=df_genres)
 
 # Example Recommendation
-# recommendations = recommend_movies(user_id=5, num_recommendations=10)
 recommendations = model.recommend_movies(user_id=5, 
                                          df_user_movie_similarities=df_user_movie_similarities, 
                                          df_ratings=df_train_ratings,
@@ -75,14 +40,5 @@ print(recommendations)
 test_recommendations = model.recommend_movies_all_users(user_movie_similarities=df_user_movie_similarities, df_ratings=df_train_ratings, df_movies=df_movies, num_recommendations=10)
 ground_truth = df_test_ratings.groupby('userId')['movieId'].apply(set).to_dict()
 
-recalls = []
-
-for user_id in test_recommendations:
-    recs = test_recommendations[user_id]
-    true_items = ground_truth.get(user_id, set())
-    score = recall_at_k(recs, true_items)
-    if score is not None:
-        recalls.append(score)
-
-average_recall = np.mean(recalls)
+average_recall = eval.average_recall_at_k(test_recommendations, ground_truth)
 print(f"Average Recall@10: {average_recall:.4f}")
